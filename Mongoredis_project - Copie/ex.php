eefa4a0e38b799f19146298b47feb159d798bf1b
sanzianap <46610258+sanzianap@users.noreply.github.com>
2023-03-03 23:19:51 +0200

    Delete ex.php

--- Mongoredis_project - Copie/ex.php
+++
@@ -1,7 +0,0 @@
-<div style="width: 20%; float:left; background-color:#ccc; height:100%">
-   #left content in here
-</div>
-
-<div style="width: 80%; float:right; background-color:#eee; height:100%">
-   #right content in there
-</div>
